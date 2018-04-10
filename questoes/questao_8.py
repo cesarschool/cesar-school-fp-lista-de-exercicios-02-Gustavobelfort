d061f20e3b135fb17c8870775dc2c951eb736564
Gustavo Belfort <gustavobelfort@outlook.com>
2018-04-10 13:11:41 -0300

    Sem importar datetime

--- questoes/questao_8.py
+++ questoes/questao_8.py
@@ -1,3 +1,4 @@
+# %load questao_8.py
 ## QUESTÃO 8 ##
 # Escreva um programa que leia uma data do usuário e calcule seu sucessor imediato.
 # Por exemplo, se o usuário inserir valores que representem 2013-11-18, seu programa 
@@ -18,13 +19,46 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 
-import datetime
+def bissexto(ano):
+    if ( ano % 4 ) == 0 :
+        return True
+    elif ( ano % 100 ) == 0 and ( ano % 400 ) == 0 :
+        return True
+    else :
+        return False
 
 def main():
-    data1 = input('Digite a data (ANO-MÊS-DIA): ')
-    data2 = datetime.datetime.strptime(data1, '%Y-%m-%d') + datetime.timedelta(days=1)
-    print(data1 + " mais um dia será : " + data2.strftime('%Y-%m-%d'))
-
+    data = input('Data:')
+    ano,mes,dia = data.split('-')
+    
+    
+    if bissexto(int(ano)) == True :
+        if dia == "29" and mes == "02" :
+            dia = "01"
+            mes = int(mes) + 1
+            
+    elif dia == "28" and mes == "02" :
+        dia = "01"
+        mes = int(mes) + 1
+    
+    
+    if (int(mes) == 1 or int(mes) == 3 or int(mes) == 5 or int(mes) == 7 or int(mes) == 8 or int(mes) == 10 or int(mes) == 12) and dia == "31" :
+        dia = "01"
+        mes = int(mes) + 1
+    
+    if (int(mes) == 4 or int(mes) == 6 or int(mes) == 9 or int(mes) == 11) and dia == "30" :
+        dia = "01"
+        mes = int(mes) + 1
+    
+    if (not (dia == "31") and not (dia == "30")) and ((not dia == "28" or not dia == "29") and (mes == "02")) :
+        dia = int(dia) + 1
+    
+    if int(mes) > 12 :
+        ano = int(ano) + 1
+        mes = 1
+        dia = 1
+        
+    print(ano,"-",mes,"-",dia)
     
 if __name__ == '__main__':
     main()
